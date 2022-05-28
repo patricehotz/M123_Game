11fcc71471805ed3098068c3ee38155c5fb7afef
roxy22r <raksana280@gmail.com>
2022-05-28 15:29:46 +0200

    RAK added Layout for Maler
    - Added code segment Layout  to get into game
    - Added Layout for Normal Player in game

---
+++ DontGuessAWordTwice/DontGuessAWordTwice/Pages/DontGuessAWordTwice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DontGuessAWordTwice.Pages
+{
+    public class guessTheWordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
